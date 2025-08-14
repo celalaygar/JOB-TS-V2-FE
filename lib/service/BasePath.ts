47983a71a9cd283bbe6baa3daa9b928724180103
hacı celal aygar <aygarcelal@gmail.com>
2025-08-14 18:12:58 +0300

    fix basepath

--- lib/service/BasePath.ts
+++ lib/service/BasePath.ts
@@ -5,23 +5,44 @@ export const REGISTER = BASE_AUTH + "register"
 export const REGISTER_WITH_TOKEN = BASE_AUTH + "register-by-invitation"
 
 
+
+
+// projects
 export const BASE_PATH_V2 = "/api/v2/";
 export const PROJECT_URL = BASE_PATH_V2 + "projects";
 export const PROJECT_USER_ROLES_URL = BASE_PATH_V2 + "project-user-roles";
 export const PROJECT_TASK_STATUS_URL = BASE_PATH_V2 + "project-task-status";
 export const PERMISSIONS = BASE_PATH_V2 + "permissions";
 
+
+
+// project-team
 export const PROJECT_TEAM_URL = BASE_PATH_V2 + "project-team";
 export const TEAM_DETAIL_URL = PROJECT_TEAM_URL + "/get-team-detail";
 export const TEAM_ALL_URL = PROJECT_TEAM_URL + "/all";
 
 
+// project-users
+export const PROJECT_USERS = BASE_PATH_V2 + "project-users";
+export const GET_ACTIVE_PROJECT_USERS = PROJECT_USERS + "/get-active-users/project";
+export const GET_ALL_PROJECT_USERS = PROJECT_USERS + "/all-users/project";
+export const REMOVE_PROJECT_USERS_URL = PROJECT_USERS + "/remove-user";
+
+
+
+
+
+// project-team-user
 export const TEAM_USER_URL = BASE_PATH_V2 + "/project-team-user";
 export const TEAM_USER_NOT_IN_URL = TEAM_USER_URL + "/not-in-team"
 export const TEAM_USER_IN_URL = TEAM_USER_URL + "/in-team"
 export const TEAM_USER_REMOVE_URL = TEAM_USER_URL + "/remove"
 
 
+
+
+
+// invitation/project
 export const INVITATION_PROJECT = BASE_PATH_V2 + "invitation/project";
 export const INVITATION_ACCEPT = INVITATION_PROJECT + "/accept";
 export const INVITATION_DECLINE = INVITATION_PROJECT + "/decline";
@@ -34,29 +55,32 @@ export const INVITATION_BY_PROJECTID = INVITATION_PROJECT + "/all-by-projectId";
 
 export const BACKLOG_URL = BASE_PATH_V2 + "backlog";
 
+
+
 export const KANBAN_URL = BASE_PATH_V2 + "kanban";
 
 
 
+
+// sprint
 export const SPRINT_URL = BASE_PATH_V2 + "sprint";
 export const SPRINT_GET_ALL_URL = SPRINT_URL + "/getAll";
 export const SPRINT_NON_COMPLETED_GET_ALL_URL = SPRINT_URL + "/non-completed/project";
 export const UPDATE_STATUS_URL = SPRINT_URL + "/status";
 
 
+
+
+// sprint-task
 export const SPRINT_TASK_URL = BASE_PATH_V2 + "sprint-task";
 export const SPRINT_TASK_ADD_URL = SPRINT_TASK_URL + "/add";
 export const SPRINT_TASK_REMOVE_URL = SPRINT_TASK_URL + "/remove";
 
-export const PROJECT_USERS = BASE_PATH_V2 + "project-users";
-export const GET_ACTIVE_PROJECT_USERS = PROJECT_USERS + "/get-active-users/project";
-export const GET_ALL_PROJECT_USERS = PROJECT_USERS + "/all-users/project";
-export const REMOVE_PROJECT_USERS_URL = PROJECT_USERS + "/remove-user";
 
 
 
 
-
+// project-task"
 export const PROJECT_TASK = BASE_PATH_V2 + "project-task"
 export const UPDATE_PROJECT_TASK_STATUS_URL = PROJECT_TASK + "/update-status";
 
